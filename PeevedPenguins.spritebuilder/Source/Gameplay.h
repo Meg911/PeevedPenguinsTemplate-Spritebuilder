894d6ca4905b1f46af8d88bac129eb52b49fc8f7
Meg911 <magreth.degraaf@y7mail.com>
2014-10-30 12:48:39 +1000

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Magreth de Graaf on 30/10/2014.
+//  Copyright (c) 2014 Software Rebel. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
